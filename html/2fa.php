0623980e4af461879595cf6039ac2bfea213fc17
Austin <astrologiesroblox@gmail.com>
2021-11-12 14:13:05 -0500

    2step prep

---
+++ html/2fa.php
@@ -0,0 +1,49 @@
+<?php
+
+if (isSession2FAUnlocked()){
+    redirect("/");
+}
+
+if(isset($_POST['submit_2fa'])) 
+{
+    attemptSession2FAUnlock($_POST['2fa_code']);
+    redirect("/");
+}
+
+$body = <<<EOT
+<form action="" method="post">
+    <div class="container" align="center" style="display: flex;justify-content: center;align-items: center;text-align: center;min-height: 100vh;">
+        <div class="card" style="max-width:38rem;">
+            <div class="card-body">
+                <div class="row">
+                    <div class="col-sm text-center">
+                        <h4>2-Step Verification</h4>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm text-center">
+                        <h1 style="font-size:14rem;"><i class="fas fa-unlock red-a-nounder"></i></h1>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm text-center">
+                        <div class="input-group">
+                            <input class="form-control" name="2fa_code" placeholder="Code" autocomplete="off">
+                            <div class="input-group-append">
+                                <button name="submit_2fa" class="btn btn-danger">Submit</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</form>
+EOT;
+
+pageHandler();
+$ph->pageTitle("2FA");
+$ph->footer = "";
+$ph->navbar = "";
+$ph->body = $body;
+$ph->output();
