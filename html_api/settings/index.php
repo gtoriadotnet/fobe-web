0161c7c6393e1d1d7f753ee3ad10f947da75f17f
Austin <astrologiesroblox@gmail.com>
2021-11-12 12:59:21 -0500

    2fa prep

--- html_api/settings/index.php
+++ html_api/settings/index.php
@@ -26,6 +26,9 @@ $joinpref = $userquery->canJoin;
 $tradepref = null;
 $theme = $userquery->theme;
 
+//initialize 2FA in the database if it hasnt been already
+initialize2FA($userid);
+
 $userInfo = array (
 	"userid" => $userid,
 	"username" => $username,
