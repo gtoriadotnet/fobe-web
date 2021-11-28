8eacfd19e3e90ef792fa009e3bf1ee4c09ca282a
Austin <astrologiesroblox@gmail.com>
2021-11-28 06:05:28 -0500

    hide guis

--- luascripts/thumbnails/PlaceScript.lua
+++ luascripts/thumbnails/PlaceScript.lua
@@ -4,6 +4,9 @@ print('Render Place ' .. assetid)
 
 game:GetService('ThumbnailGenerator').GraphicsMode = 4 
 pcall(function() game:GetService('ContentProvider'):SetBaseUrl(url) end)
-game:GetService('ScriptContext').ScriptsDisabled=true 
+game:GetService("ScriptContext").ScriptsDisabled = true
+game:GetService("StarterGui").ShowDevelopmentGui = false
 game:Load(asseturl) 
+game:GetService("ScriptContext").ScriptsDisabled = true
+game:GetService("StarterGui").ShowDevelopmentGui = false
 return game:GetService('ThumbnailGenerator'):Click(fileExtension, x, y, false)
